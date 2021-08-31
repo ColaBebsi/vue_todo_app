f38e3c00f4cbb955c44b7337bf7c3fc37302b4ff
ColaBebsi <m.samaae@hotmail.com>
2021-08-31 13:04:13 +0200

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,30 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+     <img alt="Vue logo" src="./assets/logo.png" class="logo">
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
 }
 
 #nav {
@@ -28,5 +38,11 @@
       color: #42b983;
     }
   }
+}
+
+.logo {
+  display: block;
+  margin: 20px auto;
+  height: 75px;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>todo-list</h2>
+    <input type="text" class="todo-input" placeholder="What needs to be done?">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todo-list",
+};
+</script>
+
+<style lang="scss">
+.todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+
+    &:focus {
+        outline: none;
+    }
+}
+</style>
